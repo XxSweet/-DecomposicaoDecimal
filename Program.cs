3481daf803e099a9dba48d37a12571ba982c69e1
xxsweet <xxsweet07@gmail.com>
2023-03-31 22:11:10 -0300

    Atividade De Decom.Decimal

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("--- Decomposição Decimal ---\n");
+
+Console.Write("Digite um número inteiro...: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int restante = numero;
+
+int unidades = restante % 10;
+restante /= 10;
+
+int dezenas = restante % 10;
+restante /= 10;
+
+int centenas = restante;
+
+Console.WriteLine($"\nO número {numero} possui:");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"{unidades,10} unidade(s)");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"{dezenas,10} dezenas(s)");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"{centenas,10} centena(s).");
+Console.ResetColor();
